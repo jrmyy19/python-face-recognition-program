6d9e713270103cfab0d9cb384d8ffb0770db8dae
jeremy <jeremybourgeois191@icloud.com>
2026-09-24 15:35:57 -0400

    added the text showing up on the screen and started the algorith to show different instructions after a certain amount of time to capture different angles of the user face

--- register.py
+++ register.py
@@ -2,6 +2,15 @@ from capture import open_webcam
 from capture import read_frame
 import time 
 import cv2 as cv
+
+instructions=["Look straight ahead.", 
+              "Turn your head to the left.",
+              "Turn your head to the right.", 
+              "Lift your head slightly.", 
+              "Lower your head slightly."]
+instruction_number=0
+starting_time=time.time()
+elapsed_time=time.time()-starting_time
 if __name__ == "__main__":
     video_flow=open_webcam()
     while True :
@@ -10,11 +19,19 @@ if __name__ == "__main__":
              break
         else:
             cv.putText(
-                frames,
-                "regardez tout droit",
-                (100,100),
-                cv.FONT_HERSHEY_SIMPLEX,
-                1,
-                (0,255,0),
-                2
-            )
+                    frames,
+                    instructions[instruction_number],
+                    (800,500),
+                    cv.FONT_HERSHEY_SIMPLEX,
+                    2,
+                    (0,255,0),
+                    2
+                    )
+            if elapsed_time>=6:
+                instruction_number+=1
+
+            cv.imshow("face detection",frames)
+            if cv.waitKey(1) & 0xFF == ord("q"):
+                break
+    video_flow.release()
+    cv.destroyAllWindows()
